20c1f87df333bf26f730380154cb0bed845d28d4
MariusArhaug <mariusarhaug@hotmail.com>
2021-04-18 21:55:46 +0200

    doc(main.py): add type hints to methods, add docstring to methods

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
+from __future__ import annotations
+from node import Node
 import pandas as pd
 import math
-from node import Node
 
 
-def read_file(file):
+def read_file(file: str) -> ([int], [int]):
     data = pd.read_excel(rf'{file}', sheet_name="Data", usecols=['ID', 'PARENT_ID'])
-    id_list = data['ID'].tolist()
-    parent_list = data['PARENT_ID'].tolist()
+    id_list = list(map(int, (data['ID'].tolist())))
+    parent_list = list(map(int, data['PARENT_ID'].tolist()))
     return id_list, parent_list
 
 
-def write_to_file(root, filename):
-    with open (filename, 'w') as file:
+def write_to_file(root: Node, filename: str) -> None:
+    with open(filename, 'w') as file:
         output = root.to_JSON()
         file.write(output)
 
 
-def create_node_list():
-    id_list, parent_list = read_file('Oppgave.xlsx')
-
+def create_node_list(id_list: [int], parent_list: [int]) -> (Node, [Node]):
     nodes = []
     root_node = None
     for id, parent_id in zip(id_list, parent_list):
         if math.isnan(parent_id):
             root_node = Node(id, None)
             continue
-        nodes.append(Node(id, int(parent_id)))
-    nodes = sorted(nodes, key=lambda x: x.parent_id)
+        nodes.append(Node(id, parent_id))
+    # nodes = sorted(nodes, key=lambda x: x.parent_id)
 
     return root_node, nodes
 
-def create_tree(root, node_list):
+
+def create_tree(root: Node, node_list: [Node]) -> Node:
     hashmap = {root.id: root}
 
     for node in node_list:
@@ -42,7 +42,7 @@ def create_tree(root, node_list):
     return root
 
 
-def find_path(root, start_id, end_id):
+def find_path(root: Node, start_id: int, end_id: int) -> [Node]:
     start_node = find_node(root, start_id)
     end_node = find_node(root, end_id)
     path = [start_node]
@@ -81,7 +81,7 @@ def find_way(start_node, end_id, path=[]):
 """
 
 
-def find_node(root, node_id, path=[]):
+def find_node(root: Node, node_id: int, path=[]):
     if len(path) == 0:
         path.append(root)
 
@@ -97,25 +97,21 @@ def find_node(root, node_id, path=[]):
 
     return []
 
-"""
-def find_maximum(graph, max_depth=1):
-    
-    Find deepest depth of element
-    :param graph:
-    :param max_depth:
-    :return:
-    
+
+def find_maximum(root: Node, max_depth=1) -> int:
+    """
+    Find deepest depth level of leaf nodes in a given tree
+    :param root: Root of tree
+    :param max_depth: int value of the deepest level
+    :return: max_depth: int
+    """
     new_max = max_depth
-    for child in graph:
+    for child in root:
         if new_max < child.depth and len(child.children) == 0:
             new_max = child.depth
             continue
         return find_maximum(child.children, new_max)
     return new_max
-"""
-
-def find_maximum():
-    pass
 
 
 def find_biggest_depth_node(nodes, current_node=None):
@@ -146,18 +142,10 @@ def find_nodes_at_depth(root, depth, current_nodes=[]):
 
 
 def create_hierarchy():
-    root, node_list = create_node_list()
+    root, node_list = create_node_list(read_file())
     root = create_tree(root, node_list)
     create_depth([root])
-    # write_to_file(root, 'print1.txt')
-    # print(root.to_JSON())
-    # print(find_maximum(root))
-    # path = find_node(root, 28792)
-    # for node in path:
-    #    print(node)
-    # print(find_biggest_depth_node([root]))
-    # print(find_nodes_at_depth([root], find_maximum(root)))
-    # print(find_nodes_at_depth(root, 4))
+    # write_to_file(root, 'print1.json')
 
 
 def create_depth(node_list, depth=1):
