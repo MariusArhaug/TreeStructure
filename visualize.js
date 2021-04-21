9fd2992cd183623a9e0fa873e8d3c248d8322371
MariusArhaug <mariusarhaug@hotmail.com>
2021-04-21 20:12:14 +0200

    wip(draw tree): can draw good looking tree for small json files

--- visualize.js
+++ visualize.js
@@ -1,120 +1,118 @@
 class Node {
   fixedSize = 7.5;
+  ctx = ctx;
   constructor({id, parent_id, depth, children}) {
     this.id = id;
     this.parent_id = parent_id;
     this.depth = depth;
     this.children = children;
-    this.radius = this.fixedSize + (this.fixedSize * this.children.length)/5;
-    this.x;
-    this.y;
+
+    this.radius = this.fixedSize + (this.fixedSize * this.children.length)/4;
+    this.x = canvas.width/2;
+    this.y = 100;
   }
   *[Symbol.iterator]() {
     for (let child of this.children) {
       yield child;
     }
   }
-  drawNode(x, y) {
+  updateCoordinates(x, y) {
     this.x = x;
     this.y = y;
+  }
+
+  changeCanvas(ctx) {
+    this.ctx = ctx;
+  }
+
+  drawNode() {
     ctx.beginPath();
     ctx.fillStyle = '#021225';
     ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
-    ctx.font = 'bold 20pt Calibri';
-    ctx.fill()
-    ctx.beginPath();
-    ctx.fillStyle = "white";
-    ctx.fillText(this.id, 40, 100);
     ctx.fill()
     ctx.stroke();
     ctx.closePath();
   }
 
   drawEdge(toX, toY) {   
-    const moveToX = this.x;
-    const moveToY = this.y + this.radius;
-    const lineToX = toX;
-    const lineToY = toY - this.radius;
     ctx.beginPath();
-    ctx.moveTo(moveToX, moveToY);
-    ctx.lineTo(lineToX, lineToY);
+    ctx.fillStyle = 'gray';
+    ctx.moveTo(this.x, this.y);
+    ctx.lineTo(toX, toY);
     ctx.stroke(); 
     ctx.closePath();
   };
+
+  drawID() {
+    ctx.beginPath();
+    ctx.font = 'bold 10pt Calibri';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fill()
+    ctx.beginPath();
+    ctx.fillStyle = "white";
+    ctx.fillText(this.id, this.x, this.y);
+  }
 }
 
 let canvas;
 let ctx;
-export default function visualize(JSONRoot) {
+/**
+ * Visualize a nested jsonObj into a tree with drawTree function
+ * @param {*} jsonObj nested jsonObj read from json file/API. 
+ */
+export default function visualize(jsonObj) {
   canvas = document.getElementById('canvas');
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   ctx = canvas.getContext('2d');
-  const root = new Node(JSONRoot);
-  mapJSON(root);
-  let nodes = flattenJSON(root);
-  drawTree(root, nodes);
+  let root = new Node(jsonObj)
+  root.drawNode();
+  drawTree(root);
+  root.drawID();
+}
+
+/**
+ * Draw a tree on canvas from a given root Node
+ * Split opp root's children in two halfes,
+ * so that they can be drawn from center then +/- depending on left and right.
+ * @param {*} root Root of the tree
+ * @returns undefined. 
+ */
+function drawTree(root) {
+  if (root.children.length === 0) {
+    return;
+  }
+  let children = root.children;
+  const half = Math.ceil(children.length / 2);
+  let firstHalf = children.splice(0, half);
+  let secondHalf = children.splice(-half);
   
-}
-
-function drawTree(root, nodes) {
-  const rootX = canvas.width/2
-  const rootY = 100
-  root.drawNode(rootX, rootY, ctx);
-  const max = maxDepth(nodes)
- 
-  for (let i = 2; i <= max; i++) {
-    let nodesAtDepth = nodes.filter(node => node.depth === i); 
-
-    const half = Math.ceil(nodesAtDepth.length / 2);
-    const firstHalf = nodesAtDepth.splice(0, half);
-    const secondHalf = nodesAtDepth.splice(-half);
-
-    firstHalf.map((node, j) => {
-      const parent = findParentNode(nodes, node);
-      const childX = parent.x - 40*j < 0 ? 0 : parent.x - 40*j; 
-      node.drawNode(childX,  parent.y + 60 + node.radius);
-      node.drawEdge(parent.x, parent.y);
-    });
-    secondHalf.map((node, j) => {
-      const parent = findParentNode(nodes, node);
-      const childX = parent.x + 20*(j+1) < 0 ? 0 : parent.x + 20*(j+1);
-      node.drawNode(childX, parent.y + 60 + node.radius);
-      node.drawEdge(parent.x, parent.y);
-      console.log(parent);
-    })
-    
+  firstHalf = drawNodeAndEdge(root, firstHalf, true);
+  secondHalf = drawNodeAndEdge(root, secondHalf, false);
+  children = [...[...firstHalf, ...secondHalf]];
+  for (let childNode of children) {
+    drawTree(childNode);
+    childNode.drawID();
   }
 }
-
-window.addEventListener('resize', () => resizeCanvas()) 
-
-function resizeCanvas() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-}
-
-function mapJSON(root) {
-  if (root.children.length === 0) {
-    return;
-  }
-  const children = []
-  for (let child of root) {
-    child = new Node(child)
-    children.push(child)
-    mapJSON(child)
-  }
-  root.children = children;
-}
-
-function drawEdges(root) {
-  if (root.children.length === 0) {
-    return;
-  }
-  for (let childNode of root) {
-    childNode.drawEdge(root.x, root.y);
-    drawEdges(childNode)
-  }
+/**
+ * Draw nodes in nodeArray aswell as edge from drawn node to root node. 
+ * @param {*} root Root/parent of the nodeArray
+ * @param {*} nodeArray array of children nodes of root
+ * @param {*} left decide to draw nodes either to the left or right. 
+ * @returns 
+ */
+const drawNodeAndEdge = (root, nodeArray, left) => {
+  return nodeArray.map((node, i) => {
+    node = new Node(node);
+    const x = root.x + (left ? -1 : 1 ) * (20 + node.radius*2)*(i + (left ? 0 : 1))*(1.5 * nodeArray.length * 0.5 * node.depth);
+    const y = root.y + (50 + node.radius*2) * 0.2 * node.depth;
+    node.updateCoordinates(x, y);
+    node.drawNode();
+    node.drawEdge(root.x , root.y);
+    return node;
+  })
 }
 
 function findParentNode(nodeList, node) {
